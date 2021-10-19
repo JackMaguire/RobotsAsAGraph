7c30adbda356e270a79b0b02b6dec330ddacd70a
Jack Maguire <jackmaguire1444@gmail.com>
2021-10-19 19:00:36 +0000

    train_CNN.py

--- training_scripts/train_CNN.py
+++ training_scripts/train_CNN.py
@@ -12,17 +12,23 @@ import sys
     
 def make_model():
 
-    XSize = 48
-    ESize = 32
-
     F = 9 # TODO
-    input = Input( shape=(45, 30, InputBuilder.NCnnInput ) )
+    input = Input( shape=(45, 30, F ) )
+    L = input
 
-    
+    L = Dense( 16 )( L )
+    L = Conv2D(32, kernel_size=3, strides=1, padding="valid" )( L )
+    L = Conv2D(32, kernel_size=3, strides=1, padding="valid" )( L )
+    #L = Conv2D(32, kernel_size=3, strides=1, padding="valid" )( L )
+
 
     output = Dense( 10, activation='softmax' )( L )
 
     model = Model(inputs=input, outputs=output)
-    model.compile( optimizer=adam, loss='categorical_crossentropy' )
+    model.compile( optimizer='adam', loss='categorical_crossentropy' )
 
     return model
+
+if __name__ == '__main__':
+    model = make_model()
+    model.summary()

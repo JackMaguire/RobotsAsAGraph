150ed21d069e8531216cdc514a4fd7603cf18933
Jack Maguire <jackmaguire1444@gmail.com>
2021-12-07 00:27:38 +0000

    bench

--- play_test_benchmarks.sh
+++ play_test_benchmarks.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 {
-    for x in {1..2}; do
+    for x in {1..100}; do
 	for start in 1 23 45; do
+	    mkdir play_test_benchmarks/$start 2>/dev/null
 	    for model in train_better_softmax2/nconv2.*.h5.checkpoint.h5; do
 		echo $model $start $((start+22)) $x $(basename $model)
 	    done
 	done
     done
-} | xargs -n 5 -P 24 bash -c 'python3 model_play_game.py --model $0 --start_level $1 --stop_level $2 2>/dev/null 1>play_test_benchmarks/$4.$1.$2.__.$3.log'
+} | xargs -n 5 -P 32 bash -c 'python3 model_play_game.py --model $0 --start_level $1 --stop_level $2 2>/dev/null 1>play_test_benchmarks/$1/$4.$3.log'
 
 # nconv2.3.h5.checkpoint.h5 10 67 45 train_better_softmax2/nconv2.3.h5.checkpoint.h5
 # 4                         3  2  1  0                                          

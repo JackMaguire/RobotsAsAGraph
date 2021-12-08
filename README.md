5333ca296d8b687fee245989311bb12391915bef
Jack Maguire <jackmaguire1444@gmail.com>
2021-12-08 19:26:31 +0000

    README.md

--- README.md
+++ README.md
@@ -30,4 +30,13 @@ all_data  training_data.txt  validation_data.txt
 ```sh
 mkdir train
 python3 train.py --nconv 3 --model train/model_3.h5
+
+# or...
+for y in 1 2 3 4 5; do
+    echo $y
+    for x in 1 2 3; do
+    	python3 train.py --nconv $y --model train/nconv$y.$x.h5 1>train/nconv$y.$x.log 2>train/nconv$y.$x.err &
+    done
+done 
+wait
 ```
--- play_test_benchmarks.sh
+++ play_test_benchmarks.sh
@@ -4,12 +4,13 @@
     for x in {1..100}; do
 	for start in 1 23 45; do
 	    mkdir play_test_benchmarks/$start 2>/dev/null
-	    for model in train_better_softmax2/nconv2.*.h5.checkpoint.h5; do
+	    for key in 3.1; do
+		model="train_better_softmax2/nconv${key}.h5.checkpoint.h5"
 		echo $model $start $((start+22)) $x $(basename $model)
 	    done
 	done
     done
-} | xargs -n 5 -P 32 bash -c 'python3 model_play_game.py --model $0 --start_level $1 --stop_level $2 2>/dev/null 1>play_test_benchmarks/$1/$4.$3.log'
+} | xargs -n 5 -P 48 bash -c 'python3 model_play_game.py --model $0 --start_level $1 --stop_level $2 2>/dev/null 1>play_test_benchmarks/$1/$4.$3.log'
 
 # nconv2.3.h5.checkpoint.h5 10 67 45 train_better_softmax2/nconv2.3.h5.checkpoint.h5
 # 4                         3  2  1  0                                          

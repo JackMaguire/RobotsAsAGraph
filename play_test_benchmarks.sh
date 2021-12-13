55885a1770c7e753a59af722b9c3c7c98c64d4cf
Jack Maguire <jackmaguire1444@gmail.com>
2021-12-13 00:07:40 +0000

    no more train_better_softmax2

--- play_test_benchmarks.sh
+++ play_test_benchmarks.sh
@@ -5,12 +5,12 @@
 	for start in 1 23 45; do
 	    mkdir play_test_benchmarks/$start 2>/dev/null
 	    for key in 5.3; do
-		model="train_better_softmax2/nconv${key}.h5.checkpoint.h5"
+		model="train/nconv${key}.h5.checkpoint.h5"
 		echo $model $start $((start+22)) $x $(basename $model)
 	    done
 	done
     done
 } | xargs -n 5 -P 48 bash -c 'python3 model_play_game.py --model $0 --start_level $1 --stop_level $2 2>/dev/null 1>play_test_benchmarks/$1/$4.$3.log'
 
-# nconv2.3.h5.checkpoint.h5 10 67 45 train_better_softmax2/nconv2.3.h5.checkpoint.h5
+# nconv2.3.h5.checkpoint.h5 10 67 45 train/nconv2.3.h5.checkpoint.h5
 # 4                         3  2  1  0                                          

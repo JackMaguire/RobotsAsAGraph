0f088b71f3031af72ca1d0ad23b7889230a77da6
Jack Maguire <jackmaguire1444@gmail.com>
2022-02-14 17:29:25 +0000

    larger python exposure

--- python_bindings.cc
+++ python_bindings.cc
@@ -10,10 +10,35 @@
     
 py::module m_train = m.def_submodule( "train" );
 m_train.def( "make_tensors", &make_tensors );
+m_train.def( "deserialize", &deserialize );
 
 py::class_< Tensors > tensors( m_train, "Tensors" );
 tensors.def_readonly( "input_tensors", &Tensors::input_tensors );
 tensors.def_readonly( "output_tensor", &Tensors::output_tensor );
 
+py::enum_< Key >( m_train, "Key" )
+      .value( "Q", Key::Q )
+      .value( "W", Key::W )
+      .value( "E", Key::E )
+
+      .value( "A", Key::A )
+      .value( "S", Key::S )
+      .value( "D", Key::D )
+
+      .value( "Z", Key::Z )
+      .value( "X", Key::X )
+      .value( "C", Key::C )
+
+      .value( "T", Key::T )
+      .value( "SPACE", Key::SPACE )
+      .value( "DELETE", Key::DELETE )
+
+      .value( "NONE", Key::NONE );
+
+
+py::class_< DataPoint > dp( m_train, "DataPoint" );
+dp.def_readonly( "game", &DataPoint::game );
+dp.def_readonly( "key", &DataPoint::key );
+dp.def_readonly( "level", &DataPoint::level );
 
 }
